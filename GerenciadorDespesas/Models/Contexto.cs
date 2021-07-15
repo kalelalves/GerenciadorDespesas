b00d0f74548c913252b5e59953237789ed4c5241
Kalel Jamil Alves <kalel_fipp@hotmail.com>
2021-07-14 23:46:41 -0300

    Classes OK

---
+++ GerenciadorDespesas/Models/Contexto.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Threading.Tasks;
+
+namespace GerenciadorDespesas.Models
+{
+    public class Contexto: DbContext
+    {
+        public DbSet<Meses> Meses { get; set; }
+        public DbSet<Salarios> Salarios { get; set; }
+        public DbSet<Despesas> Despesaseses { get; set; }
+        public DbSet<TipoDespesas> TipoDespesas { get; set; }
+
+        public Contexto(DbContextOptions<Contexto>opcoes):base(opcoes)
+        { 
+        }
+    }
+}
